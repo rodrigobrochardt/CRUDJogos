d1e1c229182ed1f3f8d98e6ae7effeaa4a95ac3d
rodrigobrochardt <rodrigobrochardt@gmail.com>
2021-08-22 10:10:51 -0300

    correção classe jogo

--- Modelos/Jogo.cs
+++ Modelos/Jogo.cs
@@ -21,18 +21,13 @@ namespace CRUDJogos.Modelos
 
     public override string ToString()
     {
-      string resultado = @"
-        Nome: {nome}
-        Genero: {genero}
-        Data de Lançamento: {datalançamento}
-        Sinopse: {sinopse}
+      string resultado = $@"
+        Nome: {this.Nome}
+        Genero: {this.Genero}
+        Data de Lançamento: {this.DataLançamento}
+        Sinopse: {this.Sinopse}
       ";
-
-      resultado = resultado.Replace("{nome}",this.Nome);
-      resultado = resultado.Replace("{genero}",Convert.ToString(this.Genero));
-      resultado = resultado.Replace("{datalançamento}",this.DataLançamento);
-      resultado = resultado.Replace("{sinopse}",this.Sinopse);
-
+      
       return resultado;
     }
     public string GetNome()
